0ca7a3e2f2449a56c528a113be2bf923754eddf2
Sergio <sergio.ingeo@gmail.com>
2021-09-23 22:24:18 -0500

    Conexión a la base de datos

---
+++ ciclo3backend/src/main/java/co/edu/DAO/ciclo3backend/Conexion.java
@@ -0,0 +1,41 @@
+package co.edu.DAO.ciclo3backend;
+
+import java.sql.*;
+
+public class Conexion {
+	static String db="tiendagenerica";
+	static String login ="dba";
+	static String password ="Mtic@2021";
+	
+	static String url="jdbc:mysql://localhost/"+db;
+	
+	Connection connection = null;
+	
+	/** 
+	 * Constructor de Conexion
+	 */
+	public Conexion() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			connection = DriverManager.getConnection(url,login,password);
+			//if (connection!=null) {
+			//	System.out.println("Conectado a la Base de Datos "+db+ "OK\n");
+			//}
+		}catch(SQLException e) {
+			System.out.println(e);
+		}catch(ClassNotFoundException e) {
+			System.out.println(e);
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+	
+	public void desconectar() {
+		connection =null;
+	}
+}
